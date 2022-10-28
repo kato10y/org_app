3dc27144c52fd4676df7d2435a3a4a607cf1a46e
kato10y <mishakatou.no10@gmail.com>
2022-10-29 08:24:58 +0900

    作成中 #1

---
+++ server/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>タビスケ</title>
+    <link rel="stylesheet" href="https://unpkg.com/ress@3.0.0/dist/ress.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dela+Gothic+One&family=Sawarabi+Gothic&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.1/css/all.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header class="home_header">
+        <h1>
+            <a href="index.php" class="logo_link">
+                <i class="fa-solid fa-suitcase"></i>
+                <span>タビスケ</span>
+            </a>
+        </h1>
+    </header>
+    <div class="second_header">
+        <h2>
+            計画一覧
+        </h2>
+        <ul class="icons_wrap">
+            <li>
+                <a href="" class="icon_wrap">
+                    <i class="fa-solid fa-square-plus"></i>
+                    <p>追加</p>
+                </a>
+            </li>
+            <li>
+                <a href="" class="icon_wrap">
+                    <i class="fa-solid fa-square-plus"></i>
+                    <p>追加</p>
+                </a>
+            </li>
+            <li>
+                <a href="" class="icon_wrap">
+                    <i class="fa-solid fa-square-plus"></i>
+                    <p>追加</p>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="main_content">
+        <div class="plans">
+            <article class="plan_wrap">
+                <a href="" class="plan_list">
+                    <p class="plan_day">2022/12/25〜2023/01/02</p>
+                    <p>計画タイトル</p>
+                </a>
+                <div class="action_icons">
+                    <a href="" class="plan_icon"><i class="fa-solid fa-pen-to-square"></i></a>
+                    <a href="" class="plan_icon"><i class="fa-solid fa-trash-can"></i></a>
+                </div>
+            </article>
+            <article class="plan_wrap">
+                <a href="" class="plan_list">
+                    <p class="plan_day">2022/12/25〜2023/01/02</p>
+                    <p>計画タイトル</p>
+                </a>
+                <div class="action_icons">
+                    <a href="" class="plan_icon"><i class="fa-solid fa-pen-to-square"></i></a>
+                    <a href="" class="plan_icon"><i class="fa-solid fa-trash-can"></i></a>
+                </div>
+            </article>
+            <article class="plan_wrap">
+                <a href="" class="plan_list">
+                    <p class="plan_day">2022/12/25〜2023/01/02</p>
+                    <p>計画タイトル</p>
+                </a>
+                <div class="action_icons">
+                    <a href="" class="plan_icon"><i class="fa-solid fa-pen-to-square"></i></a>
+                    <a href="" class="plan_icon"><i class="fa-solid fa-trash-can"></i></a>
+                </div>
+            </article>
+        </div>
+    </div>
+</body>
+<footer class="footer">
+    <div class="footer_flex"><i class="fa-solid fa-suitcase"></i>タビスケ Made by Yukari</div>
+</footer>
+</html>
