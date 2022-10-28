541b2281387d03ca67fc4cfba7bcbbbdf613e75f
kato10y <mishakatou.no10@gmail.com>
2022-10-29 08:48:31 +0900

    ヘッダーを分離 #1

--- server/index.php
+++ server/index.php
@@ -1,17 +1,5 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>タビスケ</title>
-    <link rel="stylesheet" href="https://unpkg.com/ress@3.0.0/dist/ress.min.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Dela+Gothic+One&family=Sawarabi+Gothic&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.1/css/all.css">
-    <link rel="stylesheet" href="css/style.css">
-</head>
+<?php include_once __DIR__ . '/_head.html' ?>
+
 <body>
     <header class="home_header">
         <h1>
