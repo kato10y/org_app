849cb53f4e053a6b521257127701a85ee26d1043
kato10y <mishakatou.no10@gmail.com>
2022-11-03 22:21:13 +0900

    修正他 #2

--- server/plan.php
+++ server/plan.php
@@ -5,38 +5,38 @@
         <h2 class="form_headline">計画登録・編集</h2>
         <form action="" method="post" class="forms">
             <div class="form_item">
-                <label for="text" name="plan_name" class="form_title">計画名</label>
-                <input type="text">
+                <label for="text" class="form_title">計画名</label>
+                <input type="text" name="plan_name" required>
             </div>
             <div class="form_item">
-                <label for="text" name="overview" class="form_title">計画概要</label>
-                <textarea name="計画概要" cols="40" rows="3"></textarea>
+                <label for="text" class="form_title">計画概要</label>
+                <textarea name="overview" cols="40" rows="3"></textarea>
             </div>
             <div class="form_item form_date">
                 <div>
-                    <label for="date" name="start_date" class="form_title">開始日</label>
-                    <input type="date">
+                    <label for="date" class="form_title">開始日</label>
+                    <input type="date" name="start_date" required>
                 </div>
                 <div>
-                    <label for="date" name="end_date" class="form_title">終了日</label>
-                    <input type="date">
+                    <label for="date" class="form_title">終了日</label>
+                    <input type="date" name="end_date" required>
                 </div>
             </div>
             <div class="form_item">
-                <label for="number" name="member" min="0" pattern="^[+-]?([1-9][0-9]*|0)$" class="form_title">参加人数</label>
-                <input type="number">
+                <label for="number" min="0" pattern="^[+-]?([1-9][0-9]*|0)$" class="form_title">参加人数</label>
+                <input type="number" name="member" required>
             </div>
             <div class="form_item">
-                <label for="number" name="cost" min="0" pattern="^[+-]?([1-9][0-9]*|0)$" class="form_title">費用</label>
-                <input type="number">
+                <label for="number" min="0" pattern="^[+-]?([1-9][0-9]*|0)$" class="form_title">費用</label>
+                <input type="number" name="cost">
             </div>
             <div class="cost_check">
-                <input type="checkbox" id="check" name="alone" required>
-                <label for="check" class="checkbox_text">一人当たりの金額です</label>
+                <input type="checkbox" id="check">
+                <label for="check" name="alone" class="checkbox_text">一人当たりの金額です</label>
             </div>
             <div class="form_item">
-                <label for="text" name="remarks" class="form_title">備考</label>
-                <textarea name="備考" cols="40" rows="3"></textarea>
+                <label for="text" class="form_title">備考</label>
+                <textarea cols="40" rows="3"></textarea>
             </div>
             <div class="form_item btns">
                 <input type="submit" class="keep_btn" value="保存">
