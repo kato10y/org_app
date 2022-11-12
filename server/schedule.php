8159c245553f558f6914d02004c642a2425fbe43
kato10y <mishakatou.no10@gmail.com>
2022-11-12 20:51:19 +0900

    css修正 #9

--- server/schedule.php
+++ server/schedule.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/functions.php';
-// データベースに接続
-$dbh = connect_db();
+require_once __DIR__ . '/config.php';
+
 ?>
 
 <!DOCTYPE html>
@@ -45,71 +45,100 @@ $dbh = connect_db();
     <div class="main_content">
         <div class="schedules">
             <article class="schedule_wrap">
-                <div class="time">
-                    <p>13:30</p>
-                    <p>〜</p>
-                    <p>15:00</p>
+                <div class="plun_subject">
+                    <div class="time">
+                        <p>15:30</p>
+                        <p>〜</p>
+                        <p>17:00</p>
+                    </div>
+                    <a href="">
+                        <i class="fa-solid fa-map"></i>
+                        シュノーケリング体験
+                    </a>
+                    <div class="right_wrap">
+                        <div class="reserve_mark already">
+                            <p>予約</p>済
+                        </div>
+                        <div class="action_icons">
+                            <a href="" class="plan_icon"><i class="fa-solid fa-pen-to-square"></i></a>
+                            <a href="" class="plan_icon"><i class="fa-solid fa-trash-can"></i></a>
+                        </div>
+                    </div>
                 </div>
-                <a href="">
-                    行動内容（シュノーケリング体験）
-                </a>
-                <div class="right_wrap">
-                    <div class="reserve_mark already">
-                        <p>予約</p>済
-                    </div>
-                    <div class="action_icons">
-                        <a href="" class="plan_icon"><i class="fa-solid fa-pen-to-square"></i></a>
-                        <a href="" class="plan_icon"><i class="fa-solid fa-trash-can"></i></a>
-                    </div>
-                </div>
-            </article>
-                        <article class="schedule_wrap">
-                <div class="time">
-                    <p>16:30</p>
-                    <p>〜</p>
-                    <p>18:00</p>
-                </div>
-                <a href="">
-                    移動（バス）
-                </a>
-                <div class="right_wrap">
-                    <div class="reserve_mark not_yet">
-                        <p>予約</p>未
-                    </div>
-                    <div class="action_icons">
-                        <a href="" class="plan_icon"><i class="fa-solid fa-pen-to-square"></i></a>
-                        <a href="" class="plan_icon"><i class="fa-solid fa-trash-can"></i></a>
-                    </div>
+                <div class="plan_detail">
+                    <div>場所</div>
+                    <div>予約担当者</div>
+                    <div>一人当たりの費用</div>
+                    <div>備考</div>
                 </div>
             </article>
-            </article>
-                        <article class="schedule_wrap">
-                <div class="time">
-                    <p>18:00</p>
-                    <p>〜</p>
-                    <p>19:00</p>
+            <article class="schedule_wrap">
+                <div class="plun_subject">
+                    <div class="time">
+                        <p>17:30</p>
+                        <p>〜</p>
+                        <p>19:00</p>
+                    </div>
+                    <a href="">
+                        <i class="fa-solid fa-train-subway"></i>
+                        市街地からバスで移動
+                    </a>
+                    <div class="right_wrap">
+                        <div class="reserve_mark unnecessary">
+                            <p>予約</p>不要
+                        </div>
+                        <div class="action_icons">
+                            <a href="" class="plan_icon"><i class="fa-solid fa-pen-to-square"></i></a>
+                            <a href="" class="plan_icon"><i class="fa-solid fa-trash-can"></i></a>
+                        </div>
+                    </div>
                 </div>
-                <a href="">
-                    ご飯
-                </a>
-                <div class="right_wrap">
-                    <div class="reserve_mark unnecessary">
-                        <p>予約</p>不要
+                <div class="plan_detail">
+                    <div>出発点</div>
+                    <div>到着点</div>
+                    <div>予約担当者</div>
+                    <div>一人当たりの費用</div>
+                    <div>備考</div>
+                </div>
+            </article>
+            <article class="schedule_wrap">
+                <div class="plun_subject">
+                    <div class="time">
+                        <p>19:00</p>
+                        <p>〜</p>
+                        <p>10:00</p>
                     </div>
-                    <div class="action_icons">
-                        <a href="" class="plan_icon"><i class="fa-solid fa-pen-to-square"></i></a>
-                        <a href="" class="plan_icon"><i class="fa-solid fa-trash-can"></i></a>
+                    <a href="">
+                        <i class="fa-solid fa-bed"></i>
+                        宿泊
+                    </a>
+                    <div class="right_wrap">
+                        <div class="reserve_mark not_yet">
+                            <p>予約</p>未
+                        </div>
+                        <div class="action_icons">
+                            <a href="" class="plan_icon"><i class="fa-solid fa-pen-to-square"></i></a>
+                            <a href="" class="plan_icon"><i class="fa-solid fa-trash-can"></i></a>
+                        </div>
                     </div>
+                </div>
+                <div class="plan_detail">
+                    <div>予約担当者<span>鈴木</span></div>
+                    <div>一人当たりの費用<span>8000円</span></div>
+                    <div>備考<span>XXXXX</span></div>
                 </div>
             </article>
         </div>
     </div>
     <div class="cost_tab">
-        <div class="cost_wrap">合計金額<p>10000円</p></div>
-        <div class="cost_wrap">１人あたり<p>2000円</p></div>
+        <div class="cost_wrap">合計金額<p>10000円</p>
+        </div>
+        <div class="cost_wrap">１人あたり<p>2000円</p>
+        </div>
     </div>
 </body>
 <footer class="footer">
     <div class="footer_flex"><i class="fa-solid fa-suitcase"></i>タビスケ Made by Yukari</div>
 </footer>
+
 </html>
